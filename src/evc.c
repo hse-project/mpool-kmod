078ab24bdad4502f511febdd20deb12f0f2e0a78
Nabeel Meeramohideen Mohamed <nmeeramohide@micron.com>
2020-04-27 11:23:05 -0500

    Wire up event counters to sysctl.

--- src/evc.c
+++ src/evc.c
@@ -6,12 +6,21 @@
 #include <linux/kernel.h>
 #include <linux/ktime.h>
 #include <linux/version.h>
+#include <linux/gfp.h>
+#include <linux/slab.h>
 
 #include <mpcore/evc.h>
+#include <mpcore/merr.h>
+#include <mpcore/mpcore_printk.h>
+#include "mpctl_params.h"
 
 static struct {
-	spinlock_t  lock;
-	struct evc *head;
+	spinlock_t                  lock;
+	struct evc                 *head;
+
+	____cacheline_aligned
+	struct ctl_table_header    *hdr;
+	struct ctl_table           *tab;
 } evc_root;
 
 
@@ -28,14 +37,111 @@ void evc_count(struct evc *evc)
 	spin_unlock(&evc_root.lock);
 }
 
-void evc_init(void)
+int
+evc_proc_handler(
+	struct ctl_table   *tab,
+	int                 write,
+	void        __user *buffer,
+	size_t             *lenp,
+	loff_t             *ppos)
 {
-	spin_lock_init(&evc_root.lock);
-	evc_root.head = NULL;
+	char       *data, *evstr = NULL, *pos;
+	const char *file;
+	struct evc *evc;
+	int         cc, rc, maxlen;
+
+	if (write)
+		return 0;
+
+	data = tab->data;
+	maxlen = tab->maxlen;
+
+	spin_lock(&evc_root.lock);
+	evc = evc_root.head;
+	spin_unlock(&evc_root.lock);
+	if (!evc)
+		goto exit;
+
+	evstr = (char *)get_zeroed_page(GFP_KERNEL);
+	if (!evstr)
+		return -ENOMEM;
+
+	pos = evstr;
+	cc = snprintf(pos, PAGE_SIZE, "%14s %6s %12s  %s",
+		      "FILE", "LINE", "ODOMETER", "FUNC");
+	pos += cc;
+	while (evc && ((pos - evstr) < PAGE_SIZE - 1)) {
+		file = strrchr(evc->evc_file, '/');
+		file = file ? file + 1 : evc->evc_file;
+
+		cc = snprintf(pos, PAGE_SIZE - (pos - evstr),
+			      "\n%14s %6d %12lu  %s",
+			      file, evc->evc_line,
+			      (ulong)atomic64_read(&evc->evc_odometer),
+			      evc->evc_func);
+
+		evc = evc->evc_next;
+		pos += cc;
+	}
+
+	if (evc)
+		mp_pr_info("Insufficient buffer space to dump all events");
+
+	tab->data = evstr;
+	tab->maxlen = PAGE_SIZE;
+
+exit:
+	rc = proc_dostring(tab, write, buffer, lenp, ppos);
+
+	tab->data = data;
+	tab->maxlen = maxlen;
+
+	if (evstr)
+		free_page((unsigned long)evstr);
+
+	return rc;
 }
 
-/* TODO: Wire this up to a sysctl proc rather than module unload...
- */
+void evc_init(void)
+{
+	struct ctl_table   *tab, *oid;
+	int                 compc, tabc, oidc;
+	merr_t              err;
+	static char        *noev = "No Events";
+
+	spin_lock_init(&evc_root.lock);
+	evc_root.head = NULL;
+
+	compc = MPC_SYSCTL_RCNT + MPC_SYSCTL_DCNT;
+	oidc  = 2;
+	tabc  = compc + oidc;
+
+	tab = kcalloc(tabc, sizeof(*tab), GFP_KERNEL);
+	if (!tab)
+		return;
+
+	oid = tab + compc;
+
+	err = mpc_sysctl_path(tab, tab + MPC_SYSCTL_RCNT, NULL, oid, NULL,
+			      MPC_SYSCTL_DMODE);
+	if (err)
+		goto errout;
+
+	mpc_sysctl_oid(oid, "events", 0444, noev, strlen(noev),
+		       evc_proc_handler);
+
+	evc_root.hdr = mpc_sysctl_register(tab);
+	if (!evc_root.hdr)
+		goto errout;
+
+	evc_root.tab = tab;
+
+	return;
+
+errout:
+	kfree(tab);
+}
+
 void evc_fini(void)
 {
 	const char *modname = "mpool";
@@ -47,7 +153,7 @@ void evc_fini(void)
 	spin_unlock(&evc_root.lock);
 
 	if (!evc)
-		return;
+		goto exit;
 
 	printk("\n%s: %14s %6s %12s  %s\n",
 	       modname, "FILE", "LINE", "ODOMETER", "FUNC");
@@ -63,4 +169,8 @@ void evc_fini(void)
 
 		evc = evc->evc_next;
 	}
+
+exit:
+	mpc_sysctl_unregister(evc_root.hdr);
+	kfree(evc_root.tab);
 }
