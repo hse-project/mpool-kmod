562fe5545711bef46559af07ab0d148ba81682d0
Nabeel Meeramohideen Mohamed <nmeeramohide@micron.com>
2020-05-14 18:11:45 -0500

    Remove restrictive distro checks from CMake.
    (cherry picked from commit 767d4fbff4f7126d54d17000c30583aafc2be38e)

--- src/include/mpcore/mpcore_printk.h
+++ src/include/mpcore/mpcore_printk.h
@@ -10,19 +10,13 @@
 
 #include <mpcore/merr.h>
 
-#ifdef MPOOL_DISTRO_EL7
-#define MPNL "\n"
-#else
-#define MPNL
-#endif
-
 /* TODO: Use dev_crit(), dev_err(), ... */
 
 #define mp_pr_crit(_fmt, _err, ...)				\
 do {								\
 	char errbuf[128];					\
 								\
-	printk(KERN_CRIT "%s: " _fmt ": %s" MPNL,		\
+	printk(KERN_CRIT "%s: " _fmt ": %s",		        \
 	       __func__, ## __VA_ARGS__,			\
 	       merr_strinfo((_err), errbuf, sizeof(errbuf)));	\
 } while (0)
@@ -31,25 +25,25 @@ do {								\
 do {								\
 	char errbuf[128];					\
 								\
-	printk(KERN_ERR "%s: " _fmt ": %s" MPNL,		\
+	printk(KERN_ERR "%s: " _fmt ": %s",		        \
 	       __func__, ## __VA_ARGS__,			\
 	       merr_strinfo((_err), errbuf, sizeof(errbuf)));	\
 } while (0)
 
 #define mp_pr_warn(_fmt, ...)					\
-	printk(KERN_WARNING "%s: " _fmt MPNL, __func__, ## __VA_ARGS__)
+	printk(KERN_WARNING "%s: " _fmt, __func__, ## __VA_ARGS__)
 
 #define mp_pr_notice(_fmt, ...)					\
-	printk(KERN_NOTICE "%s: " _fmt MPNL, __func__, ## __VA_ARGS__)
+	printk(KERN_NOTICE "%s: " _fmt, __func__, ## __VA_ARGS__)
 
 #define mp_pr_info(_fmt, ...)					\
-	printk(KERN_INFO "%s: " _fmt MPNL, __func__, ## __VA_ARGS__)
+	printk(KERN_INFO "%s: " _fmt, __func__, ## __VA_ARGS__)
 
 #define mp_pr_debug(_fmt, _err, ...)				\
 do {								\
 	char errbuf[128];					\
 								\
-	printk(KERN_DEBUG "%s: " _fmt ": %s" MPNL,		\
+	printk(KERN_DEBUG "%s: " _fmt ": %s",			\
 	       __func__, ## __VA_ARGS__,			\
 	       merr_strinfo((_err), errbuf, sizeof(errbuf)));	\
 } while (0)
@@ -65,7 +59,7 @@ do {								\
 								\
 	if (printk_timed_ratelimit(&state, dly)) {		\
 		merr_strinfo((_err), errbuf, sizeof(errbuf));	\
-		printk(KERN_ERR "%s: " _fmt ": %s" MPNL,	\
+		printk(KERN_ERR "%s: " _fmt ": %s",		\
 		       __func__, ## __VA_ARGS__, errbuf);	\
 	}							\
 } while (0)
